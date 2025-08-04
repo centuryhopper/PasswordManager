9c4c4d67d7311e2ddbceee26aa8a333313be557e
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2025-08-04 01:41:01 -0400

    jwt now has a 30 day lifetime vs previous 7 day

--- Web_App/Server/Models/JwtConfig.cs
+++ Web_App/Server/Models/JwtConfig.cs
@@ -5,6 +5,6 @@ public static class JwtConfig
 {
     public static readonly string JWT_TOKEN_NAME = "JWT_TOKEN_NAME";
     public static readonly string JWT_TOKEN_EXP_DATE_NAME = "JWT_TOKEN_EXP_DATE_NAME";
-    public static DateTime JWT_TOKEN_EXP_DATETIME => DateTime.UtcNow.AddDays(7);
+    public static DateTime JWT_TOKEN_EXP_DATETIME => DateTime.UtcNow.AddDays(30);
 }
 
