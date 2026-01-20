4437fc95a35a9df20b5bdd1c5e57f3925c2fa815
Leo Zhang <leo_zhang@pop-os.lan>
2026-01-20 13:15:49 -0500

    update

--- Web_App/Server/Models/ServiceResponses.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Shared.Models;
-
-public class ServiceResponses
-{
-    public record class GeneralResponse(bool Flag, string Message);
-    public record class GeneralResponseWithPayload(bool Flag, string Message, string Payload);
-    public record class LoginResponse(bool Flag, string Token, string Message);
-}
